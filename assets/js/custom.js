32e8d7fd7f282013119c858959b9cae6c743c175
denish.mistry <denish.mistry@indianic.com>
2022-11-23 18:16:53 +0530

    custom js change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -167,11 +167,11 @@ setTimeout(function () {
   gsap.set("main", {
     opacity: 1,
   });
-//
-    // gsap.utils.toArray("main").forEach(box => {
-        // gsap.utils.toArray(".blog-post ,.popular-post .section-title-large").forEach(box => {
-//  gsap.utils.toArray(".blog-post ,.popular-post .section-title-large").forEach(box => {
-gsap.utils.toArray("main").forEach((box) => {
+  //
+  // gsap.utils.toArray("main").forEach(box => {
+  // gsap.utils.toArray(".blog-post ,.popular-post .section-title-large").forEach(box => {
+  //  gsap.utils.toArray(".blog-post ,.popular-post .section-title-large").forEach(box => {
+  gsap.utils.toArray("main").forEach((box) => {
     gsap.set(box, {
       y: 100,
       ease: 'power1.out',
@@ -181,7 +181,7 @@ gsap.utils.toArray("main").forEach((box) => {
         trigger: box,
         markers: false,
         scrub: 4,
-    
+
       },
       ease: 'power1.out',
       y: 0,
@@ -209,10 +209,10 @@ function ResponsiveImg(imgname, imgwidth, imgheight) {
 
 
 //=========================
-    //  Header Sticky top
+//  Header Sticky top
 // ==========================
 
-const showAnim = gsap.from('header', { 
+const showAnim = gsap.from('header', {
   yPercent: -100,
   paused: true,
   duration: 0.2
@@ -231,55 +231,55 @@ ScrollTrigger.create({
 //  Home section
 // ================
 
-if(window.innerWidth > 1200){
+if (window.innerWidth > 1200) {
   gsap.utils.toArray(".move-content").forEach(box => {
     gsap.to(box, {
-         opacity:0,
-         scrollTrigger: {
-              trigger:box,
-             //  pin:box,
-              markers: false,  
-              scrub:1 ,
-              start: "40 200",
-              end: "+=80",
-         }
-     });
-   });
-   gsap.utils.toArray(".move-picture").forEach(box => {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: box,
+        //  pin:box,
+        markers: false,
+        scrub: 1,
+        start: "40 200",
+        end: "+=80",
+      }
+    });
+  });
+  gsap.utils.toArray(".move-picture").forEach(box => {
     gsap.to(box, {
-         scrollTrigger: {
-              trigger:box,
-              scrub:1 ,
-              scale: 1,
-              markers:false,
-              start: "100 200",
-              end: "+=100",
-         },
-         zoom:0.7,
-         x:"-50%", 
-         y:"150",
-     });
-   });
+      scrollTrigger: {
+        trigger: box,
+        scrub: 1,
+        scale: 1,
+        markers: false,
+        start: "100 200",
+        end: "+=100",
+      },
+      zoom: 0.7,
+      x: "-50%",
+      y: "150",
+    });
+  });
 }
 
 
 
 gsap.utils.toArray(".whatido").forEach((box) => {
   gsap.set(box, {
-    y:50,
-    opacity:0.5,
+    y: 50,
+    opacity: 0.5,
   });
   gsap.to(box, {
     scrollTrigger: {
       trigger: box,
       markers: false,
       scrub: 1,
-  
+
       start: "30% 100%",
       end: "50% 100%",
     },
-    opacity:1,
-    y:0
+    opacity: 1,
+    y: 0
   });
 });
 
@@ -289,102 +289,138 @@ gsap.utils.toArray(".whatido").forEach((box) => {
 // expiriance section
 // =====================
 
-if(window.innerWidth > 1200){
-gsap.utils.toArray(".expirince-list li").forEach((box) => {
+if (window.innerWidth > 1200) {
+  gsap.utils.toArray(".expirince-list li").forEach((box) => {
 
-  gsap.to(box, {
-    scrollTrigger: {
-      trigger: box,
-      markers: false,
-      scrub: 2,
-      toggleClass: "active",
-      start: "0% 60%",
-      end: "100% 60%",
-    },
+    gsap.to(box, {
+      scrollTrigger: {
+        trigger: box,
+        markers: false,
+        scrub: 2,
+        toggleClass: "active",
+        start: "0% 60%",
+        end: "100% 60%",
+      },
 
+    });
   });
-});
 
 
-// gsap.utils.toArray(".expirince-list li div").forEach((box) => {
-//   gsap.set(box, { 
+  // gsap.utils.toArray(".expirince-list li div").forEach((box) => {
+  //   gsap.set(box, {
 
-//     skewType:"simple",
-//     skewX:-90,
-//     opacity:0,
+  //     skewType:"simple",
+  //     skewX:-90,
+  //     opacity:0,
 
-//   });
-//   gsap.to(box, {
-//     scrollTrigger: {
-//       trigger: box,
-//       markers: false,
-//       scrub: 4,
-//       start: "0% 70%", 
-//       end: "10% 70%",
-//     },
-//     skewX:0,
-//     opacity:1,
-//   });
-// });
+  //   });
+  //   gsap.to(box, {
+  //     scrollTrigger: {
+  //       trigger: box,
+  //       markers: false,
+  //       scrub: 4,
+  //       start: "0% 70%",
+  //       end: "10% 70%",
+  //     },
+  //     skewX:0,
+  //     opacity:1,
+  //   });
+  // });
 }
 
 // =============
 // brain Image
 // =============
 
-function BrainImage(){
+function BrainImage() {
   const doc = this.getSVGDocument();
   const brain = doc.querySelector("#brain");
   const topbrain = doc.querySelector("#topline");
   const bottombrain = doc.querySelector("#bottomline");
   const brainmaintop = doc.querySelector("#top-mainline");
   const outline = doc.querySelector("#outerline");
-  
+
   const triggerselector = document.querySelector(".expirince");
   gsap.utils.toArray(topbrain).forEach((box) => {
-    gsap.set(box, { strokeDasharray: 330,strokeDashoffset: 330,opacity:0.3 });
+    gsap.set(box, {
+      strokeDasharray: 330,
+      strokeDashoffset: 330,
+      opacity: 0.3
+    });
     gsap.to(box, {
-      scrollTrigger: {trigger: triggerselector, markers: false, scrub: 4, start: "0% 50%", end: "30% 30%", },
+      scrollTrigger: {
+        trigger: triggerselector,
+        markers: false,
+        scrub: 4,
+        start: "0% 50%",
+        end: "30% 30%",
+      },
 
-      strokeDashoffset:0,
+      strokeDashoffset: 0,
     });
   });
 
   gsap.utils.toArray(brainmaintop).forEach((box) => {
-    gsap.set(box, {strokeDasharray: 164, strokeDashoffset: 164,opacity:0.3});
+    gsap.set(box, {
+      strokeDasharray: 164,
+      strokeDashoffset: 164,
+      opacity: 0.3
+    });
     gsap.to(box, {
-      scrollTrigger: {trigger: triggerselector, markers: false, scrub: 4, start: "40% 50%", end: "100% 30%", },
-      strokeDashoffset:0,
-      opacity:0.3
+      scrollTrigger: {
+        trigger: triggerselector,
+        markers: false,
+        scrub: 4,
+        start: "40% 50%",
+        end: "100% 30%",
+      },
+      strokeDashoffset: 0,
+      opacity: 0.3
     });
   });
   gsap.utils.toArray(outline).forEach((box) => {
-    gsap.set(box, { 
+    gsap.set(box, {
       strokeDasharray: 880,
-      strokeDashoffset: 880,  
-      opacity:0.3 
+      strokeDashoffset: 880,
+      opacity: 0.3
     });
     gsap.to(box, {
-      scrollTrigger: {trigger: triggerselector, markers: false, scrub: 4, start: "40% 80%", end: "90% 50%", },
-      strokeDashoffset:0,
+      scrollTrigger: {
+        trigger: triggerselector,
+        markers: false,
+        scrub: 4,
+        start: "40% 80%",
+        end: "90% 50%",
+      },
+      strokeDashoffset: 0,
     });
   });
 
   gsap.utils.toArray(bottombrain).forEach((box) => {
-    gsap.set(box, {strokeDasharray: 227, strokeDashoffset: 227,opacity:0.3 });
+    gsap.set(box, {
+      strokeDasharray: 227,
+      strokeDashoffset: 227,
+      opacity: 0.3
+    });
     gsap.to(box, {
-      scrollTrigger: {trigger: triggerselector, markers: false, scrub: 4, start: "70% 80%", end: "90% 50%", },
+      scrollTrigger: {
+        trigger: triggerselector,
+        markers: false,
+        scrub: 4,
+        start: "70% 80%",
+        end: "90% 50%",
+      },
 
-      strokeDashoffset:0,
+      strokeDashoffset: 0,
     });
   });
 }
 
 const refs = [...document.querySelectorAll(`[data-svg]`)];
 for (let i = 0; i < refs.length; i++) {
-    refs[i].addEventListener("load", window[refs[i].getAttribute("data-svg")]);
-    // console.log(refs[i].getAttribute("data-svg"));
-    // console.log(window[refs[i].getAttribute("data-svg")]);
+  refs[i].addEventListener("load", window[refs[i].getAttribute("data-svg")]);
+  // console.log(refs[i].getAttribute("data-svg"));
+  // console.log(window[refs[i].getAttribute("data-svg")]);
 }
 
 // var path = document.querySelector('#topline');
@@ -392,7 +428,7 @@ for (let i = 0; i < refs.length; i++) {
 // console.log(length);
 
 // stroke-dasharray: 878;
-// 
+//
 
 // strokeDasharray
 // strokeDashoffset
@@ -405,4 +441,3 @@ for (let i = 0; i < refs.length; i++) {
 // TweenMax.set(path, {attr:{pathLength: 1}});
 
 // svg Animation
-
